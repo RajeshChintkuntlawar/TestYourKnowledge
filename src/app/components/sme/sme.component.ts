a728c2152c447e90639c7a8c5884ac996357c981
Islam, Imrana <imrana.islam@capitalone.com>
2017-11-03 02:20:35 -0400

    added SME landing page

--- src/app/components/sme/sme.component.ts
+++ src/app/components/sme/sme.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sme',
-  templateUrl: './sme.component.html'
+  templateUrl: './sme.component.html',
+  styleUrls: ['sme.component.css']
 })
 export class SmeComponent {
 
