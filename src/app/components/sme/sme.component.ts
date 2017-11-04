801509cb4ded1179948bd05a9f47cba4dd068a04
Islam, Imrana <imrana.islam@capitalone.com>
2017-11-04 01:42:02 -0400

    Added rbutton click redirects for SME landing page

--- src/app/components/sme/sme.component.ts
+++ src/app/components/sme/sme.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sme',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['sme.component.css']
 })
 export class SmeComponent {
+  constructor(private router: Router) {
+  }
 
+  private signOut() {
+    this.router.navigate(['login']);
+    }
+  private  viewSmeQueizzs() {
+    this.router.navigate(['sme/smeviewquizzes']);
+    }
+   private  smeViewQueizzes() {
+    this.router.navigate(['sme/smeviewquizzes']);
+    }
+
+  private  smeAddQueiz() {
+    this.router.navigate(['sme/smeaddquiz']);
+    }
+
+  private  smeUpdateQueiz() {
+    this.router.navigate(['sme/smeupdatequiz']);
+    }
+
+  private  smeDeleteQueiz() {
+    this.router.navigate(['sme/smedeletequiz']);
+    }
 }
