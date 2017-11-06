0a9d71dd8c51e181447e32d0ebe2ef85038d9aed
Gadipati <devi.gadipati@capitalone.com>
2017-11-06 12:07:57 -0500

    login page integrated with spring MVC

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+private username;
+private password;
+private headers;
+
+public ngOnInit() {
+}
+
+constructor(private http: Http) {
+}
+
+public loginSubmit() {
+ this.headers = new Headers();
+ this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
+ this.http.post('http://localhost:8081/test-your-knowledge/login'
+  , `username=${this.username}&password=${this.password}`, {headers: this.headers})
+  .subscribe(res => {
+  }, err => console.log(err));
+}
 
 }
