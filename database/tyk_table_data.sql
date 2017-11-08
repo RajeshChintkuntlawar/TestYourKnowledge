5dd30711caa4aaccbaff6d867b23f3be4e300f02
RajeshChintkuntlawar <chraaj@gmail.com>
2017-11-07 23:51:37 -0500

    More updates

--- database/tyk_table_data.sql
+++ database/tyk_table_data.sql
@@ -8,53 +8,54 @@ INSERT INTO roles(
 INSERT INTO users(
 	user_name, password, last_name, first_name, email_id, role_id)
     values
-('rajesh', 'test', 'chintkuntlawar', 'rajesh', 'rajesh.chintkuntlawar@capitalone.com',  1),
-('imrana', 'test', 'islam', 'imrana', 'imrana.islam@capitalone.com',  1),
-('brunda', 'test', 'prakash', 'brunda', 'brunda.prakash@capitalone.com',  1),
-('matt', 'test', 'dolan', 'matt', 'matt.dolan@capitalone.com', 2),
-('devi', 'test', 'gadipati', 'devi', 'devi.gadipati@capitalone.com', 2);
+('rajesh', 'test', 'Chintkuntlawar', 'Rajesh', 'rajesh.chintkuntlawar@capitalone.com',  1),
+('imrana', 'test', 'Islam', 'Imrana', 'imrana.islam@capitalone.com',  1),
+('brunda', 'test', 'Prakash', 'Brunda', 'brunda.prakash@capitalone.com',  1),
+('matt', 'test', 'Dolan', 'Matt', 'matt.dolan@capitalone.com', 2),
+('devi', 'test', 'Gadipati', 'Devi', 'devi.gadipati@capitalone.com', 2);
 
 INSERT INTO quiz_type(
-    quiz_type_title, quiz_type_description)
-	VALUES ('Java', 'Technology'),
-    ('Movies', 'Entertainment'),
-    ('Football', 'Sports'),
-    ('American', 'Food');
+    quiz_subject_area, quiz_description)
+	VALUES ('Technology','Core Java Functionality'),
+    ('Entertainment','Movies'),
+    ('Sports','Football'),
+    ('Food','American Cuisine'),
+    ('Food','Italian Cuisine');
 
-INSERT INTO complexity( complexity_level_description)
-	VALUES ('easy'),('moderate'),('hard');
+INSERT INTO question_complexity(question_complexity_level_description)
+	VALUES ('Easy'),('Moderate'),('Hard');
     
-INSERT INTO question(question_text, question_complexity_id, quiz_type_id)
-	VALUES ('Which of these keywords is used to make a class?',10,13),
-    ('Which of the following is a valid declaration of an object of class Box?', 10,13),
-    ('Which of these statement is incorrect?',11,13),
-    ('Which of the following statements is correct?',11,13),
-    ('What is the stored in the object obj in following lines of code? box obj;',12,13),
-    ('Which of these operators is used to allocate memory for an object?',12,13);
+INSERT INTO questions(question_text, question_complexity_id, quiz_type_id)
+	VALUES ('Which of these keywords is used to make a class?',1,1),
+    ('Which of the following is a valid declaration of an object of class Box?', 1,1),
+    ('Which of these statement is incorrect?',2,1),
+    ('Which of the following statements is correct?',2,1),
+    ('What is the stored in the object obj in following lines of code? box obj;',3,1),
+    ('Which of these operators is used to allocate memory for an object?',3,1);
     
-INSERT INTO answer(
+INSERT INTO answers(
 	answer_id, answer_option_text, answer_option_validity_flag, question_id)
-	VALUES (1, 'class', 'Y', 19),
-    (2, 'claass', 'N', 19),
-    (3, 'function', 'N', 19),
-    (4, 'None of the above mentioned', 'N', 19),
-    (1, 'Box obj = new Box();', 'Y', 20),
-    (2, 'Box obj = new Box;', 'N', 20),
-    (3, 'obj = new Box();', 'N', 20),
-    (4, 'new Box obj;', 'N', 20),
-    (1, 'Applets do not require a main() method at all.', 'N', 21),
-    (2, 'Every class must contain a main() method.', 'Y', 21),
-    (3, 'There can be only one main() method in a program.', 'N', 21),
-    (4, 'main() method must be made public.', 'N', 21),
-    (1, 'Public method is accessible only to subclasses of its parent class', 'N', 22),
-    (2, 'Public method can only be called by object of its class.', 'N', 22),
-    (3, 'Public method is accessible to all other classes in the hierarchy', 'Y', 22),
-    (4, 'Public method can be accessed by calling object of the public class.', 'N', 22),
-    (1, 'Memory address of allocated memory of object.', 'N', 23),
-    (2, 'NULL', 'Y', 23),
-    (3, 'Any arbitrary pointer', 'N', 23),
-    (4, 'Garbage', 'N', 23),
-    (1, 'malloc', 'N', 24),
-    (2, 'alloc', 'N', 24),
-    (3, 'new', 'Y', 24),
-    (4, 'give', 'N', 24);
+	VALUES (1, 'class', 'Y', 1),
+    (2, 'claass', 'N', 1),
+    (3, 'function', 'N', 1),
+    (4, 'None of the above mentioned', 'N', 1),
+    (1, 'Box obj = new Box();', 'Y', 2),
+    (2, 'Box obj = new Box;', 'N', 2),
+    (3, 'obj = new Box();', 'N', 2),
+    (4, 'new Box obj;', 'N', 2),
+    (1, 'Applets do not require a main() method at all.', 'N', 3),
+    (2, 'Every class must contain a main() method.', 'Y', 3),
+    (3, 'There can be only one main() method in a program.', 'N', 3),
+    (4, 'main() method must be made public.', 'N', 3),
+    (1, 'Public method is accessible only to subclasses of its parent class', 'N', 4),
+    (2, 'Public method can only be called by object of its class.', 'N', 4),
+    (3, 'Public method is accessible to all other classes in the hierarchy', 'Y', 4),
+    (4, 'Public method can be accessed by calling object of the public class.', 'N', 4),
+    (1, 'Memory address of allocated memory of object.', 'N', 5),
+    (2, 'NULL', 'Y', 5),
+    (3, 'Any arbitrary pointer', 'N', 5),
+    (4, 'Garbage', 'N', 5),
+    (1, 'malloc', 'N', 6),
+    (2, 'alloc', 'N', 6),
+    (3, 'new', 'Y', 6),
+    (4, 'give', 'N', 6);
